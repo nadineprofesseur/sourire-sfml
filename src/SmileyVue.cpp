191a4c6b9b3f07058813c519de73df7a9379e91c
nadineducegep <st-amand.nadine@cgmatane.qc.ca>
2017-11-14 15:16:47 -0500

    Delta

--- src/SmileyVue.cpp
+++ src/SmileyVue.cpp
@@ -13,19 +13,19 @@ SmileyVue::SmileyVue(int x, int y) {
 
     visage = new CircleShape(50.f);
     visage->setFillColor(Color::Yellow);
-    visage->setPosition(50,50);
+    visage->setPosition(x + 50,y + 50);
 
     oeilDroit = new CircleShape(10.f);
     oeilDroit->setFillColor(Color::Green);
-    oeilDroit->setPosition(110,70);
+    oeilDroit->setPosition(x + 110,y + 70);
 
     oeilGauche = new CircleShape(10.f);
     oeilGauche->setFillColor(Color::Green);
-    oeilGauche->setPosition(70,70);
+    oeilGauche->setPosition(x + 70,y + 70);
 
     bouche = new RectangleShape(Vector2f(70, 5));
     bouche->setFillColor(Color::Red);
-    bouche->setPosition(65,110);
+    bouche->setPosition(x + 65,y + 110);
 }
 
 SmileyVue::SmileyVue() {
